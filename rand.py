1160b14c9c5643269a397f843a892c04e476cc0e
李心汝 <lixinru@lixinrudeAir.Home>
2021-11-14 11:37:46 +0800

    origin version

---
+++ rand.py
@@ -0,0 +1,14 @@
+import random
+
+r = random.randint(1,100)
+while True:
+	num = input('請輸入一個數字： ')
+	num = int(num)
+	if num == r:
+		print('終於猜對了')
+		break
+	else:
+		if num > r:
+			print('比答案大')
+		else:
+			print('比答案小')
