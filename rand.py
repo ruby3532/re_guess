8d08df9a22344c9bd7edf11f8f366bf0639392f9
李心汝 <lixinru@lixinrudeAir.Home>
2021-11-14 11:56:05 +0800

    add count

--- rand.py
+++ rand.py
@@ -1,13 +1,15 @@
 import random
 
 r = random.randint(1,100)
+i = 0 # 猜幾次
 while True:
 	num = input('請輸入一個數字： ')
 	num = int(num)
+	i = i + 1 # 可以寫： i+=1
 	if num == r:
-		print('終於猜對了')
+		print('終於猜對了, 這是你猜的第', i , '次')
 		break
 	elif num > r:  # 可以用 elif
-		print('比答案大')
+		print('比答案大, 這是你猜的第', i , '次')
 	elif num < r:
-		print('比答案小')
+		print('比答案小, 這是你猜的第', i , '次')
