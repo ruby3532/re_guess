11f925f6c971978a7cbcb04d6e35a4aaa92ee2d7
李心汝 <lixinru@lixinrudeAir.Home>
2021-11-14 11:59:10 +0800

    整理重複性問題

--- rand.py
+++ rand.py
@@ -7,9 +7,11 @@ while True:
 	num = int(num)
 	i = i + 1 # 可以寫： i+=1
 	if num == r:
-		print('終於猜對了, 這是你猜的第', i , '次')
+		print('終於猜對了')
+		print('這是你猜的第', i , '次')
 		break
 	elif num > r:  # 可以用 elif
-		print('比答案大, 這是你猜的第', i , '次')
+		print('比答案大')
 	elif num < r:
-		print('比答案小, 這是你猜的第', i , '次')
+		print('比答案小')
+	print('這是你猜的第', i , '次')
